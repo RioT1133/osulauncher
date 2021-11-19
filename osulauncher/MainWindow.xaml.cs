cebde9d44ed0413d69b41d2ce7a111fe189396bf
riot1133 <riot1133@outlook.com>
2021-11-19 18:25:04 +0200

    added songs folder selection prompt

--- osulauncher/MainWindow.xaml.cs
+++ osulauncher/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -18,11 +19,31 @@ namespace osulauncher
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
     public partial class MainWindow : Window
     {
+        private FolderBrowserDialog songsFolderDialog;
+        private System.Windows.Forms.Label selectedPath;
+        private string songsFolder;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.songsFolderDialog = new FolderBrowserDialog();
+            songsFolderDialog.Description = "Select osu! Songs folder";
+            songsFolderDialog.RootFolder = Environment.SpecialFolder.LocalApplicationData;
+            DialogResult result = songsFolderDialog.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                this.songsFolder = songsFolderDialog.SelectedPath;
+                folderLabel.Content = "Selected folder: " + this.songsFolder;
+                folderLabel.FontSize = 12;
+            }
         }
     }
 }
