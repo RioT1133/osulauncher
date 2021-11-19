24a7a00e9598d3047c9e540236638e1d14884c76
riot1133 <riot1133@outlook.com>
2021-11-19 21:35:37 +0200

    added config file creation with songs folder path

--- osulauncher/MainWindow.xaml.cs
+++ osulauncher/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,25 +26,60 @@ namespace osulauncher
     {
         private FolderBrowserDialog songsFolderDialog;
         private System.Windows.Forms.Label selectedPath;
-        private string songsFolder;
+        private string songsFolder = "not set";
+        private string defaultSongsFolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "/osu!/Songs";
+        private string configFile = Environment.CurrentDirectory + "/config.txt";
+        private string[] config;
 
         public MainWindow()
         {
             InitializeComponent();
+            if(!File.Exists(configFile)) //if config file hasn't been created yet
+            {
+                //if default folder exists, set it as songs folder
+                if (Directory.Exists(defaultSongsFolder))
+                {
+                    songsFolder = defaultSongsFolder;
+                }
+                folderLabel.Content = "Songs folder: " + songsFolder;
+                makeConfigFile();
+            } else //if there is a config file
+            {
+                updateConfigFile();
+            }
         }
 
+        //choose folder
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             this.songsFolderDialog = new FolderBrowserDialog();
             songsFolderDialog.Description = "Select osu! Songs folder";
-            songsFolderDialog.RootFolder = Environment.SpecialFolder.LocalApplicationData;
             DialogResult result = songsFolderDialog.ShowDialog();
             if (result == System.Windows.Forms.DialogResult.OK)
             {
                 this.songsFolder = songsFolderDialog.SelectedPath;
-                folderLabel.Content = "Selected folder: " + this.songsFolder;
-                folderLabel.FontSize = 12;
             }
+        }
+
+        private void makeConfigFile()
+        {
+            using (System.IO.FileStream fs = System.IO.File.Create(configFile))
+            {
+                string songsPath = "songsPath:" + songsFolder;
+                AddText(fs, songsPath);
+            }
+        }
+
+        private void updateConfigFile()
+        {
+            this.config = File.ReadAllLines(configFile);
+            this.songsFolder = String.Join("", config[0].Split(':').Skip(0));
+        }
+
+        private static void AddText(FileStream fs, string value)
+        {
+            byte[] info = new UTF8Encoding(true).GetBytes(value);
+            fs.Write(info, 0, info.Length);
         }
     }
 }
